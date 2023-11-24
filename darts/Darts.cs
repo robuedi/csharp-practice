2de047b8882d4fdf489b4f3f68d71945209a1a6c
Eduard Robu <robu.edi1@gmail.com>
2023-11-24 14:06:03 +0200

    darts

--- darts/Darts.cs
+++ darts/Darts.cs
@@ -4,6 +4,31 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+       if(Darts.insideRange(x,y,1))
+       {
+            return 10;
+       }
+
+       if(Darts.insideRange(x,y,5))
+       {
+            return 5;
+       }
+
+       if(Darts.insideRange(x,y,10))
+       {
+            return 1;
+       }
+
+       return 0;
+    }
+
+    private static bool insideRange(double x, double y, int radius)
+    {
+       if((x*x)+(y*y) <= radius*radius)
+       {
+            return true;
+       }
+
+       return false;
     }
 }
