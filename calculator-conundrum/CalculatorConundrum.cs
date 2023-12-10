de53367bdad964e4afca50d7dd76a6b1e6180c9f
Eduard Robu <robu.edi1@gmail.com>
2023-12-10 22:00:14 +0200

    calculator-conundrum

---
+++ calculator-conundrum/CalculatorConundrum.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static class SimpleCalculator
+{
+    public static string Calculate(int operand1, int operand2, string operation) => operation switch { 
+        "+" => $"{operand1} + {operand2} = {SimpleOperation.Addition(operand1, operand2)}",
+        "*" => $"{operand1} * {operand2} = {SimpleOperation.Multiplication(operand1, operand2)}",
+        "/" => operand2 == 0 ? "Division by zero is not allowed." :  $"{operand1} / {operand2} = {SimpleOperation.Division(operand1, operand2)}",
+        null => throw new ArgumentNullException(),
+        "" => throw new ArgumentException(),
+        _ => throw new ArgumentOutOfRangeException()
+    };
+}
