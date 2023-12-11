d348d3b8c480948a005f6eb8593c8630c9320dd7
Eduard Robu <robu.edi1@gmail.com>
2023-12-11 15:38:27 +0200

    attach of the trolls

--- attack-of-the-trolls/AttackOfTheTrolls.cs
+++ attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,28 +1,30 @@
 using System;
 
-// TODO: define the 'AccountType' enum
+enum AccountType {
+    Guest, User, Moderator
+}
 
-// TODO: define the 'Permission' enum
+[Flags]
+enum Permission{
+    Read = 1, 
+    Write = 2, 
+    Delete = 4, 
+    All = 7, 
+    None = 0
+}
 
 static class Permissions
 {
-    public static Permission Default(AccountType accountType)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
-    }
+    public static Permission Default(AccountType accountType) => accountType switch {
+        AccountType.Guest => Permission.Read,
+        AccountType.User => Permission.Read | Permission.Write,
+        AccountType.Moderator => Permission.All,
+        _ => Permission.None
+    };
 
-    public static Permission Grant(Permission current, Permission grant)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Grant() method");
-    }
+    public static Permission Grant(Permission current, Permission grant) => current | grant;
 
-    public static Permission Revoke(Permission current, Permission revoke)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Revoke() method");
-    }
+    public static Permission Revoke(Permission current, Permission revoke) => current &~ revoke;
 
-    public static bool Check(Permission current, Permission check)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Check() method");
-    }
+    public static bool Check(Permission current, Permission check) => (current & check) == check;
 }
