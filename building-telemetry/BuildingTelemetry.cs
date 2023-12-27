1d94027f3395d431537970947f223088a9abd11b
Eduard Robu <robu.edi1@gmail.com>
2023-12-27 11:07:05 +0200

    building telemetry

---
+++ building-telemetry/BuildingTelemetry.cs
@@ -0,0 +1,59 @@
+using System;
+
+public class RemoteControlCar
+{
+    private int batteryPercentage = 100;
+    private int distanceDrivenInMeters = 0;
+    private string[] sponsors = new string[0];
+    private int latestSerialNum = 0;
+
+    public void Drive()
+    {
+        if (batteryPercentage > 0)
+        {
+            batteryPercentage -= 10;
+            distanceDrivenInMeters += 2;
+        }
+    }
+
+    public void SetSponsors(params string[] sponsors) => this.sponsors = sponsors;
+
+    public string DisplaySponsor(int sponsorNum) => this.sponsors[sponsorNum];
+
+    public bool GetTelemetryData(ref int serialNum,
+        out int batteryPercentage, out int distanceDrivenInMeters)
+    {
+        if(serialNum < this.latestSerialNum)
+        {
+            serialNum = this.latestSerialNum;
+            batteryPercentage = -1;
+            distanceDrivenInMeters = -1;
+            return false;
+        }
+
+        this.latestSerialNum = serialNum;
+        batteryPercentage = this.batteryPercentage;
+        distanceDrivenInMeters = this.distanceDrivenInMeters;
+        return true;
+    }
+
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+}
+
+public class TelemetryClient
+{
+    private RemoteControlCar car;
+
+    public TelemetryClient(RemoteControlCar car)
+    {
+        this.car = car;
+    }
+
+    public string GetBatteryUsagePerMeter(int serialNum) =>
+        this.car.GetTelemetryData(ref serialNum, out var batteryPercentage, out var distanceDrivenInMeters) == false || distanceDrivenInMeters == 0 
+            ? "no data" 
+            : $"usage-per-meter={(100 - batteryPercentage)/distanceDrivenInMeters}";
+}
