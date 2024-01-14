d40c1a3eeafc332e6255b80523ac532fc997b98f
Eduard Robu <robu.edi1@gmail.com>
2024-01-14 10:51:30 +0000

    phone number analysis

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,13 +2,7 @@ using System;
 
 public static class PhoneNumber
 {
-    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
-    }
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) => (IsNewYork: phoneNumber[0..3] == "212", IsFake: phoneNumber[4..7] == "555", LocalNumber: phoneNumber[^4..]);
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
 }
